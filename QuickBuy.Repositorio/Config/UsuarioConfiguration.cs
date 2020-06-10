640c2bf4dce94e1e568fdab8d7d489d56fb2eb53
Willian Rébulla Gomes <willian.gomes@ms.senac.br>
2020-06-09 20:43:28 -0400

    Datasets e DataContexts

---
+++ QuickBuy.Repositorio/Config/UsuarioConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickBuy.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Repositorio.Config
+{
+    public class UsuarioConfiguration : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Usuario> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
