e30b75a34cc2306d3f9ca8f82511227293533210
Willian Rébulla Gomes <willian.gomes@ms.senac.br>
2020-06-10 20:57:10 -0400

    Parte 1 de Configuration

--- QuickBuy.Repositorio/Config/UsuarioConfiguration.cs
+++ QuickBuy.Repositorio/Config/UsuarioConfiguration.cs
@@ -10,7 +10,31 @@ namespace QuickBuy.Repositorio.Config
     {
         public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Usuario> builder)
         {
-            throw new NotImplementedException();
+            builder.HasKey(u => u.Id);
+
+            // Builder usa padrao Fluent
+            builder
+                .Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder
+                .Property(u => u.Senha)
+                .IsRequired()
+                .HasMaxLength(400);
+
+            builder
+                .Property(u => u.Nome)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder
+               .Property(u => u.SobreNome)
+               .IsRequired()
+               .HasMaxLength(50);
+
+
+
+
         }
     }
 }
